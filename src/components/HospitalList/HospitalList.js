a708e956dc9b3405c314b9895b56f89654909de7
yyounghoon <yhyoon@huray.net>
2022-01-22 22:12:32 +0900

    test commit

--- src/components/HospitalList/HospitalList.js
+++ src/components/HospitalList/HospitalList.js
@@ -4,7 +4,7 @@ import './HospitalList.scss';
 
 function HospitalListItem({ data }) {
   const [isModalOn, setIsModalOn] = useState(false);
-  const [asdfaasdfasdfsdfasdfasdf, setTest] = useState('false');
+  const [asdfaasdfasdasdffsdfasdfasdf, setTest] = useState('false');
 
   const handleModal = () => {
     setIsModalOn(!isModalOn);
